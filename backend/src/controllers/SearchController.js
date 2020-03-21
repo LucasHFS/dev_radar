dc7cb1f1dafc8f683cb726d6193a0748f8cea105
lucashfs <lucash.silva1612@gmail.com>
2020-03-21 01:45:56 -0300

    initial commit

---
+++ backend/src/controllers/SearchController.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+const Dev = require('../models/Dev');
+
+const parseStringAsArray = require('../utils/parseStringAsArray')
+
+module.exports = {
+    async index(request, response){
+        //Buscar todos devs num raio de 10km
+        //Filtrar por Techs
+
+        const { latitude, longitude, techs } = request.query;
+
+        const techsArray = parseStringAsArray(techs);
+
+        const devs = await Dev.find({
+            techs: {
+                $in: techsArray,
+            },
+            location: {
+                $near: {
+                    $geometry:{
+                        type: 'Point',
+                        coordinates: [longitude, latitude],
+                    },
+                    $maxDistance: 10000,
+                }
+            }
+        });
+
+        return response.json({devs});
+    }
+}
